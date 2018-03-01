60b0e3f659d76d0a66e6b394a17b5db6e921b10a
Jarek Figura <jarek.figura@icloud.com>
2018-03-01 16:43:17 +0100

    Lesson 10

--- test.js
+++ test.js
@@ -1,13 +1,9 @@
-// *** Lesson 9 *** //
+// *** Lesson 10 *** //
 
-var myVar = 10;     console.log(myVar); // 10
-myVar = myVar + 5;  console.log(myVar); // 15
-
-myVar += 5;   console.log(myVar); // 20
-myVar -= 10;  console.log(myVar); // 10
-myVar /= 2;   console.log(myVar); // 5
-myVar *= 5;   console.log(myVar); // 25
-
-myVar += 1;   console.log(myVar); // 26
-myVar++;      console.log(myVar); // 27
-++myVar;      console.log(myVar); // 28
+var myVar = "Shaun";
+document.write(myVar);  // "Shaun"
+console.log(myVar);     // "Shaun"
+document.write(10);     // "Shaun10"
+console.log(50);        // 50
+console.log(20 * 5);    // 100
+document.write(20 / 5); // "Shaun104"
