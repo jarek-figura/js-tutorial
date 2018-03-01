9f5e47923dd8d8beaf5edb9bbf33de177eefd666
Jarek Figura <jarek.figura@icloud.com>
2018-03-01 16:29:12 +0100

    Lesson 8

--- test.js
+++ test.js
@@ -9,14 +9,26 @@ alert("Hello, World!");
 // move the image from top to bottom
 // make the image visible
 
-var maVariableShaun;
-maVariableShaun = 10;
+// *** Lesson 7 *** //
 
-var myVar2 = "hello";
-myVar2 = "hello again";
-myVar2 = 5;
+var myVariable;  console.log(myVariable); // undefined
+myVariable = 10; console.log(myVariable); // 10
 
-var customerAge = 32;
+var myVar2 = "hello";   console.log(myVar2);  // "hello"
+myVar2 = "hello again"; console.log(myVar2);  // "hello again"
+myVar2 = 5;             console.log(myVar2);  // 5
 
-var myVar3 = 40;
+var customerAge = 32; console.log(customerAge); // 32
+var myVar3 = 40;      console.log(myVar3);      // 40
+
+// *** Lesson 8 *** //
+
+var myVar = 5;      console.log(myVar); // 5
+myVar = myVar + 10; console.log(myVar); // 15
+myVar = myVar - 5;  console.log(myVar); // 10
+myVar = myVar / 2;  console.log(myVar); // 5
+
+myVar = 5 + "hello";        console.log(myVar); // "5hello"
+myVar = "hello" + " david"; console.log(myVar); // "hello david"
+myVar = 5 * "hello";        console.log(myVar); // NaN
 
