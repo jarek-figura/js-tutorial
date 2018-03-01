3c04764ee73b01e02b384d82aebcb69632dc206e
Jarek Figura <jarek.figura@icloud.com>
2018-03-01 16:36:29 +0100

    Lesson 9

--- test.js
+++ test.js
@@ -1,34 +1,13 @@
-alert("Hello, World!");
-// 1-line comment
+// *** Lesson 9 *** //
 
-/*
-* n-line comment
-*/
+var myVar = 10;     console.log(myVar); // 10
+myVar = myVar + 5;  console.log(myVar); // 15
 
-// make the image invisible
-// move the image from top to bottom
-// make the image visible
+myVar += 5;   console.log(myVar); // 20
+myVar -= 10;  console.log(myVar); // 10
+myVar /= 2;   console.log(myVar); // 5
+myVar *= 5;   console.log(myVar); // 25
 
-// *** Lesson 7 *** //
-
-var myVariable;  console.log(myVariable); // undefined
-myVariable = 10; console.log(myVariable); // 10
-
-var myVar2 = "hello";   console.log(myVar2);  // "hello"
-myVar2 = "hello again"; console.log(myVar2);  // "hello again"
-myVar2 = 5;             console.log(myVar2);  // 5
-
-var customerAge = 32; console.log(customerAge); // 32
-var myVar3 = 40;      console.log(myVar3);      // 40
-
-// *** Lesson 8 *** //
-
-var myVar = 5;      console.log(myVar); // 5
-myVar = myVar + 10; console.log(myVar); // 15
-myVar = myVar - 5;  console.log(myVar); // 10
-myVar = myVar / 2;  console.log(myVar); // 5
-
-myVar = 5 + "hello";        console.log(myVar); // "5hello"
-myVar = "hello" + " david"; console.log(myVar); // "hello david"
-myVar = 5 * "hello";        console.log(myVar); // NaN
-
+myVar += 1;   console.log(myVar); // 26
+myVar++;      console.log(myVar); // 27
+++myVar;      console.log(myVar); // 28
