d07df6ccbef3c044b9f397ec2a2584dc340ec288
Jarek Figura <jarek.figura@icloud.com>
2018-03-02 19:50:38 +0100

    Lesson 21

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
-// *** Lesson 20 *** //
+// *** Lesson 21 *** //
 
-function getAverage (a, b, c, d, e, f) {
-	var average = (a + b + c + d + e + f) / 6;
+function getAverage (a, b) {
+	var average = (a + b) / 2; // local variable
 	console.log(average);
 	return average;
 }
 
-var myResult = getAverage(7, 8, 9, 10, 11, 12);
+var myResult = getAverage(7, 11); // global variable
 
-console.log("the average is " + myResult);
+function logResult () {
+	console.log("the 'average' is " + myResult + " inside the function");
+}
+
+logResult();
