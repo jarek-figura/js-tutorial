cd828b144506e221df190d17f5df28ec1631a495
Jarek Figura <jarek.figura@icloud.com>
2018-03-03 11:13:03 +0100

    Lesson 34

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-// *** Lesson 33 *** //
+// *** Lesson 34 *** //
 
 // DOM (Document Object Model)
 // Document is a representation of HTML
@@ -6,11 +6,11 @@
 // Model is a tree representation of Objects
 // Node is everything we can change in the Document (Element, Text, HTML attributes)
 
-// Traversing the DOM
+var myBody = document.getElementsByTagName("body");   console.log(myBody);
+// console.log(myBody[0].innerHTML);
 
-var myContentDivs = document.getElementsByClassName("content");   console.log(myContentDivs);
+// myBody[0].innerHTML = "<p>I am a paragraph tag</p>";
 
-var myH2 = myContentDivs[1].getElementsByTagName("h2");           console.log(myH2);
-myH2[0].innerHTML = "YO NINJAS";
-
-var myIDs = document.getElementById("page-title");                console.log(myIDs);
+var myTitle = document.getElementById("page-title");  console.log(myTitle);
+console.log(myTitle.textContent);
+myTitle.textContent = "Yo Ninjas, get ready to learn!";
